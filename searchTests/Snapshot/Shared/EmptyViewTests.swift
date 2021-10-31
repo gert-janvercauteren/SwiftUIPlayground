4de82f5dd9dbf977d18dd942cfc4c664adac0f25
Gert-Jan Vercauteren <gert-jan@ihavegotaticket.com>
2021-10-31 12:15:52 +0800

    test: add right to left test

--- searchTests/Snapshot/Shared/EmptyViewTests.swift
+++ searchTests/Snapshot/Shared/EmptyViewTests.swift
@@ -7,6 +7,10 @@ import SnapshotTesting
 @testable import search
 
 class EmptyViewTests: XCTestCase {
+    override func setUpWithError() throws {
+        try? super.setUpWithError()
+        isRecording = false
+    }
     
     func test_emptyFavoritesView() {
         // Given
@@ -23,5 +27,16 @@ class EmptyViewTests: XCTestCase {
         
         let traitDarkMode = UITraitCollection(userInterfaceStyle: .dark)
         assertSnapshot(matching: host, as: .image(size: host.intrinsicContentSize, traits: traitDarkMode))
+        
+        
+    }
+    
+    func test_emptyFavoritesView_rightToLeft() {
+        // Given
+        let sut = EmptyView.emptyFavorites
+        let host: UIView = UIHostingController(rootView: sut).view
+        
+        let traitRtl = UITraitCollection(layoutDirection: .rightToLeft)
+        assertSnapshot(matching: host, as: .image(size: host.intrinsicContentSize, traits: traitRtl))
     }
 }
